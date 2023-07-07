512cce27669a24ef328441c612448fa31552f865
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2023-07-07 14:31:56 +0600

    read product in google sheets

--- sheets_logic/view.py
+++ sheets_logic/view.py
@@ -12,16 +12,24 @@ credentials = sac.from_json_keyfile_name(credentials_path, scope)
 client = gspread.authorize(credentials)
 
 
-async def gsheet2df(spreadsheet_name, sheet_num):
+async def gsheet2df(spreadsheet_name, sheet_num) -> dict:
+    result: dict = {}
+    try:
+        sheet = client.open(spreadsheet_name).get_worksheet(sheet_num).get_all_records(numericise_ignore=['all'])
+        df = pd.DataFrame.from_dict(sheet)
+        result["sheet"] = sheet
+        result["df"] = df
+        result["status"] = "success"
+    except Exception as ex:
+        result["error"] = ex
+        result["status"] = "error"
 
-    sheet = client.open(spreadsheet_name).get_worksheet(sheet_num).get_all_records(numericise_ignore=['all'])
-    df = pd.DataFrame.from_dict(sheet)
-    return sheet
+    return result
 
 
-async def kaspi_umag_name():
-    kaspi_umag_sheets_name = "Названия Каспи-Umag"
-    result = {}
+async def kaspi_umag_name() -> dict:
+    kaspi_umag_sheets_name: str = "Названия Каспи-Umag"
+    result: dict = {}
 
     try:
         result_sheet = await gsheet2df(kaspi_umag_sheets_name, 0)
@@ -34,14 +42,18 @@ async def kaspi_umag_name():
     return result
 
 
-async def product_name_kaspi():
-    stock_sheets_name = "Склад_export_1678877246390_testing"
-    result = {}
+async def product_name_kaspi() -> dict:
+    stock_sheets_name: str = "Склад_export_1678877246390_testing"
+    result: dict = {}
 
     try:
         result_sheet = await gsheet2df(stock_sheets_name, 0)
-        result["data"] = {i["Штрихкод"]: i["Название товара "] for i in result_sheet}
-        result["status"] = "success"
+        if result_sheet["status"] == "success":
+            result["data"] = {i["Штрихкод"]: i["Название товара "] for i in result_sheet["sheet"]}
+            result["status"] = "success"
+        else:
+            result["error"] = result_sheet["error"]
+            result["status"] = "error"
     except Exception as ex:
         result["error"] = ex
         result["status"] = "error"
@@ -49,25 +61,48 @@ async def product_name_kaspi():
     return result
 
 
-async def count_product_kaspi():
-    stock_sheets_name = "Склад_export_1678877246390_testing"
-    result = {"data_product": []}
+async def count_product_kaspi() -> dict:
+    stock_sheets_name: str = "Склад_export_1678877246390_testing"
+    result: dict = {"data_product": []}
     try:
         result_sheet = await gsheet2df(stock_sheets_name, 0)
-        for product in result_sheet:
-            if int(product["Кол-во"])<=0:
-                result["data_product"].append(product)
-        if len(result["data_product"]) == 0:
-            result["count_del"] = 0
+        if result_sheet["status"] == "success":
+            for product in result_sheet["sheet"]:
+                if int(product["Кол-во"]) <= 0:
+                    result["data_product"].append(product)
+            if len(result["data_product"]) == 0:
+                result["count_del"] = 0
+            else:
+                result["count_del"] = len(result["data_product"])
+            result["status"] = "success"
         else:
-            result["count_del"] = len(result["data_product"])
-        result["status"] = "success"
+            result["error"] = result_sheet["error"]
+            result["status"] = "error"
     except Exception as ex:
         result["error"] = ex
         result["status"] = "error"
+
+    return result
+
+
+async def info_del_product() -> dict:
+    name_umag: dict = await kaspi_umag_name()
+    del_product: dict = await count_product_kaspi()
+    result: dict = {
+        "ls_del_product": []
+    }
+
+    if del_product["status"] == "success":
+        if del_product["count_del"] > 0:
+            if name_umag["status"] == "success":
+
+                print(del_product["data_product"])
+        result["status"]: str = "success"
+    else:
+        result["status"]: str = "error"
 
     return result
 
 
 def main():
-    print(asyncio.run(count_product_kaspi()))
+    asyncio.run(info_del_product())
